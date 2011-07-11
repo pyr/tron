0b61df5b192579aa49274cdbf69985c01eaa4087
Pierre-Yves Ritschard <pyr@milestonelab.com>
2011-07-11 15:50:26 +0200

    add URL

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject tron "0.5.0"
   :description "TRON: Task Run ON"
+  :url "https://github.com/pyr/tron"
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[lein-marginalia "0.6.0"]])
