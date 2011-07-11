f8081f084f4748ae75cd2ce5cbc77d38645dc505
Pierre-Yves Ritschard <pyr@milestonelab.com>
2011-07-11 15:46:08 +0200

    add marginalia as a development dependency.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject tron "0.5.0"
   :description "TRON: Task Run ON"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dev-dependencies [[lein-marginalia "0.6.0"]])
