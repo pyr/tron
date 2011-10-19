0df1608f4ae552214f83dd225032b8932f0aa66e
Pierre-Yves Ritschard <pyr@spootnik.org>
2011-10-19 11:00:38 +0300

    move to 1.3

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject tron "0.5.1"
   :description "TRON: Task Run ON"
   :url "https://github.com/pyr/tron"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[lein-marginalia "0.6.0"]])
